c68288974168740199d73360ae71d9362e88280b
Ajmalshk04 <shaikhajmal50@gmail.com>
2025-02-27 07:51:57 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://dreamy-palmier-5b5309.netlify.app'],
+  origin: ['https://auth-testing-b4ia.onrender.com'],
   credentials: true
 }));
 
