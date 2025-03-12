3985683dd2f1c94cec0b103c987583c21b767a48
Ajmalshk04 <shaikhajmal50@gmail.com>
2025-03-12 12:22:22 +0530

    cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', process.env.CLIENT_URL],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
